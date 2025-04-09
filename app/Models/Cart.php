75abb6d7171d52c0b8ec5bdc4a820b9ce67383f6
Eslam Gaber <esslam556547@gmail.com>
2025-04-09 07:56:28 +0200

    make relationship of cart

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -4,8 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str ;
 
 class Cart extends Model
 {
     use HasFactory;
+
+    protected $fillable =['cookie_id' ,'user_id' ,'product_id' ,'quantity' ,'options'];
+
+    public $incrementing = false ;
+
+    protected $keyType='string';
+
+
+    public static function booted(){
+
+
+        static::creating(function( Cart $cart){
+
+            $cart->id=Str::uuid();
+        });
+    }
+
+    //En.Safady follows that cart one to one relationship cart and product
+
+    public function product(){
+        return $this->belongsTo(Product::class,'product_id','id');
+    }
+
+    //user has one cart
+    public function user(){
+        return $this->belongsTo(User::class ,'user_id','id')->withDefault( //with default you need to tell if no user make the name of user 'anonymous' and  don't tell 
+                                                                            //error access brobery on null
+           [ 'name'=>'anonymous']
+        );
+    }
 }
